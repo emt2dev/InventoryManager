d2f19fa0fb9a36a64806d3d3c73b2f1e0607a4a0
David Duron <86013234+emt2dev@users.noreply.github.com>
2023-09-06 11:58:14 -0400

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,43 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+validatePrice = function ()
+{
+    let price = $(#price).val();
+    let regex = /^[1-9]\d*(((,\d{3}){1})?(\.\d{0,2})?)$/;
+    if (regex.test(price)) {
+        //Input is valid, check the number of decimal places
+        var twoDecimalPlaces = /\.\d{2}$/g;
+        var oneDecimalPlace = /\.\d{1}$/g;
+        var noDecimalPlacesWithDecimal = /\.\d{0}$/g;
+
+        if (value.match(twoDecimalPlaces)) {
+            //all good, return as is
+            let validPrice = true;
+            $(#price).val(price);
+        }
+        if (value.match(noDecimalPlacesWithDecimal)) {
+            //add two decimal places
+            price = price + '00';
+            let validPrice = true;
+            $(#price).val(price);
+        }
+        if (value.match(oneDecimalPlace)) {
+            //ad one decimal place
+            price = price + '0';
+            let validPrice = true;
+            $(#price).val(price);
+        }
+        //else there is no decimal places and no decimal
+        price = price + '.00';
+        let validPrice = true;
+        $(#price).val(price);
+    }
+
+    price = 0.00;
+    let validPrice = true;
+    $(#price).val(price);
+
+    if (validPrice) $(#NewItemForm).submit();
+}
